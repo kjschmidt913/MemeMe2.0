1c28df2357021ca93fdf6a04be11823228bf8b5a
KJ <kj.schmidt913@gmail.com>
2018-01-25 20:15:01 -0500

    table and collection view working. meme generator working. toggling after sharing is working. Just had an issue with Xcode not finding all the files? everything builds and runs, but i can't commit all the files

--- Meme Me 2.0/ViewController.swift
+++ Meme Me 2.0/ViewController.swift
@@ -6,20 +6,4 @@
 //  Copyright © 2018 KJ Schmidt. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
 
